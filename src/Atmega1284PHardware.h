cb3f263a3e8b6ba0743a41b0d40ed33a1df47cb7
Tom Malone <tmalone+git@gmail.com>
2015-01-12 18:45:21 +0000

    In addition to the components of rosserial_client, you must provide some code that connects rosserial to your hardware platform. This is done in the form of Atmega1284PHardware.h for this project. The requirements for connecting rosserial to your hardware platform include creating an interface that permits ROS to read and write to your device (hence the name of the package, rosserial_client), and to get the time in milliseconds since the start of the ROS program runtime. That's what this file does.

---
+++ src/Atmega1284PHardware.h
@@ -0,0 +1,47 @@
+#ifndef _ATMEGA1284P_HARDWARE_H_
+#define _ATMEGA1284P_HARDWARE_H_
+
+extern "C"
+{
+	#include <avr/interrupt.h>
+	#include <avr/wdt.h>
+	#include "avr_time.h"
+	#include "avr_uart.h"
+}
+
+class Atmega1284PHardware {
+	public:
+		Atmega1284PHardware() {}
+
+		// Initialize the AVR
+		void init()
+		{
+			wdt_diable();
+			avr_time_init();
+			avr_uart_init();
+			sei();
+		}
+
+		// Read a byte of data fromn the ROS connection
+		// If no data, returns -1
+		int read()
+		{
+			return avr_uart_receive_byte();
+		}
+
+		// Send a byte of data to the ROS connection
+		void write(uint8_t* data, int length)
+		{
+			for (int i = 0; i < length; i++) {
+				avr_uart_send_bte(data[i]);
+			}
+		}
+
+		// Returns millseconds since start of program
+		unsigned long time()
+		{
+			return avr_time_now();
+		}
+}
+
+#endif
